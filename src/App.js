a3f4699e2b927b67779eccdce0d563d5321c26f9
nowsirasak <nowsirasak@gmail.com>
2026-07-12 23:22:21 +0700

    Update App.js with css ecternal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import './style.css';
 
 function App() {
-  const divStyle = {
-    color: 'red',
-    backgroundColor: 'powerblue',
-    fontSize: 'larger',
-    padding: '3px',
-  };
-  const el = (
-    <div style={divStyle}>
-      Hello React
-    </div>
-  );
-  return el;
+  return (
+    <>
+      <h2>DeveloperThai.com</h2>
+      <div className="title">React & React Native</div>
+    </>
+  )
 }
 
 export default App;
