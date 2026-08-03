28690f3ac6eaad99842817a67a9655261edf91cc
nowsirasak <nowsirasak@gmail.com>
2026-08-03 10:22:07 +0700

    calculator
    calculator source code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Header, Content, Footer } from "./func-components";
 import Calendar from "./class-components";
+import { Calculator2 } from "./calculator";
+
+
 function App() {
-    return (
-        <>
-
-            <Header />
-            <p><center><Calendar /></center></p>
-            <Content />
-
-            <Footer />
-
-        </>
+  return (
+    <>
+      <Calculator2/>
+    </>
     );
 }
 
