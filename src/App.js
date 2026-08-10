4115fb18ebd57240389c820d09bee4ccb4df2ad9
nowsirasak <nowsirasak@gmail.com>
2026-08-10 10:48:47 +0700

    Refs Array
    Refs Array Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import RefsFunc from './refs-func'
+import RefsArray from './refs-array'
 
 function App() {
-    return <RefsFunc/>
+    return <RefsArray/>
 }
 
 export default App
